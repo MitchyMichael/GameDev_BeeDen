22fdabec629daf63ed5a01b5163be3489d6d2a76
jverdianto <73513762+jverdianto@users.noreply.github.com>
2022-12-28 15:19:26 +0700

    wave system, random spawn added

---
+++ Assets/Script/RandomSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float delay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(enemyWave());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnEnemy(GameObject enemy)
+    {
+        // GameObject newEnemy = Instantiate(enemyPrefab) as GameObject;
+        // newEnemy.transform.position = new Vector3(12, Random.Range(-3, 3), 0);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(10, Random.Range(-3f, 3), 0), Quaternion.identity);
+    }
+
+    IEnumerator enemyWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delay);
+            spawnEnemy(enemyPrefab);
+        }
+    }
+}
